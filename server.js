d16e6b78805c9966908ff173f749fbd556686766
nabil <mynameisnabil09@gmail.com>
2025-04-08 15:13:44 +0300

    update routes

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const studyMaterialRoutes = require('./src/routes/studyMaterialRoutes');
 
 
 app.use(express.json());
-app.use('/api/admin', authRoutes);
-app.use('/api/data_admin', adminRoutes);
+app.use('/api/super_admin', authRoutes);
 app.use('/app-status', appStatusRoutes);
+app.use('/api/admin/data_admin', adminRoutes);
 app.use('/api/admin/data_candidates', candidateRoutes);
 app.use('/api/admin/study_material', studyMaterialRoutes);
 
